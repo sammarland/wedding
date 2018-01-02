d5f51622cec90deb82711ae2e494b063cfdf94da
Sam Marland <sammarland@gmail.com>
2018-01-02 18:34:11 +0000

    Adding styles

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/style/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname + '/style/style.css'));
+});
+
 app.listen(PORT);
