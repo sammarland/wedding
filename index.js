d96c9a0515ac105b65980cd1aa874f37d481b742
Sam Marland <sammarland@gmail.com>
2018-01-05 17:15:24 +0000

    Added openshift port binding

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const PORT = process.env.PORT || 5000
 
-// viewed at http://localhost:8080
+const PORT = process.env.OPENSHIFT_NODEJS_IP || process.env.PORT || 5000;
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
